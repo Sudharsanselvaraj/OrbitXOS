cb94ba787eb31149d970558bbaa3fce02cf100bf
Sudharsan <ss0856@srmist.edu.in>
2025-08-19 19:16:36 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,10 +8,20 @@ def root():
     return {"message": "🚀 Satellite Collision Risk API is running"}
 
 @app.get("/predict")
-def predict(i_ecc: float, j_ecc: float, i_incl: float, j_incl: float, vrel_kms: float):
+def predict(
+    i_ecc: float,
+    j_ecc: float,
+    i_incl: float,
+    j_incl: float,
+    vrel_kms: float
+):
     """
+    Predict satellite collision risk.
     Example:
     /predict?i_ecc=0.01&j_ecc=0.02&i_incl=55&j_incl=56&vrel_kms=10
     """
-    return predict_risk(i_ecc, j_ecc, i_incl, j_incl, vrel_kms)
-
+    try:
+        result = predict_risk(i_ecc, j_ecc, i_incl, j_incl, vrel_kms)
+        return {"status": "success", "result": result}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}

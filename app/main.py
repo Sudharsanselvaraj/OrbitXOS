e45bc894d770b23482af3b1da6f22359ed22aae7
Sudharsan <ss0856@srmist.edu.in>
2025-08-20 23:02:27 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.model import predict_top_events
 
 app = FastAPI(title="🚀 Satellite Collision Risk API")
+
+# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],    # Allow all origins - everyone can access
+    allow_origins=["*"],    # Allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 @app.get("/")
 def root():
     return {"message": "API is running", "endpoints": ["/predict"]}
-
 
 @app.get("/predict")
 def predict():
